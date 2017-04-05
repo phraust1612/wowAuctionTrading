ffaa2adc549b14455e0e892330901f6e06216a7d
Min ByeongUk <phraust@kaist.ac.kr>
2017-04-05 18:59:25 +0900

    use selenium to perform faster

--- cacheAuction.py
+++ cacheAuction.py
@@ -1,12 +1,8 @@
-import requests as rq
+from bs4 import BeautifulSoup
+from selenium import webdriver
 import time
 import json
 import sys
-
-setregion="none"
-if len(sys.argv)==2:
-	setregion = sys.argv[1]
-	print("set region : "+setregion)
 
 f=open("realms","r")
 s=f.read()
@@ -14,6 +10,11 @@ f.close()
 js=json.loads(s)
 keys=list(js.keys())
 keys.sort()
+driver = webdriver.Chrome("C:/chromedriver_win32/chromedriver.exe")
+
+setregion=keys
+if len(sys.argv)>=2:
+	setregion = sys.argv[1:]
 
 def CacheOne(i):
 	for j in js[i]:
@@ -21,21 +22,26 @@ def CacheOne(i):
 		print("saving : "+i+" - "+name+"...")
 		AH=j[name]
 		try:
-			res = rq.get(AH)
+			driver.get(AH)
 		except:
 			print("failed!!!")
 			continue
+		page = driver.page_source
+		soup = BeautifulSoup(page,"html.parser")
+		res = soup.text
+		res = res.replace("\n","")
+		res = res.replace("\t","")
+		resjs = json.loads(res)
+		res = json.dumps(resjs)
 		updatetime = int(time.time())
 		title = "data/"+i+"_"+name+"_"+str(updatetime)+".txt"
 		with open(title,"w") as f:
-			f.write(res.text)
+			f.write(res)
 
 while True:
 	t1 = time.time()
-	if setregion in keys:
-		CacheOne(setregion)
-	else:
-		for i in keys:
+	for i in setregion:
+                if i in keys:
 			CacheOne(i)
 	t2 = time.time()
 	dt = t2 - t1
