40661d2b1605e8cb03b3d5fe611b826dab5660a6
Min ByeongUk <phraust@kaist.ac.kr>
2017-04-05 19:30:50 +0900

    error

--- cacheAuction.py
+++ cacheAuction.py
@@ -10,43 +10,41 @@ f.close()
 js=json.loads(s)
 keys=list(js.keys())
 keys.sort()
-driver = webdriver.Chrome("C:/chromedriver_win32/chromedriver.exe")
-
 setregion=keys
 if len(sys.argv)>=2:
-	setregion = sys.argv[1:]
+        setregion = sys.argv[1:]
+
+driver = webdriver.Chrome("C:/chromedriver_win32/chromedriver.exe")
 
 def CacheOne(i):
 	for j in js[i]:
-		name=list(j.keys())[0]
-		print("saving : "+i+" - "+name+"...")
-		AH=j[name]
-		try:
-			driver.get(AH)
-		except:
-			print("failed!!!")
-			continue
-		page = driver.page_source
-		soup = BeautifulSoup(page,"html.parser")
-		res = soup.text
-		res = res.replace("\n","")
-		res = res.replace("\t","")
-		resjs = json.loads(res)
-		res = json.dumps(resjs)
-		updatetime = int(time.time())
-		title = "data/"+i+"_"+name+"_"+str(updatetime)+".txt"
-		with open(title,"w") as f:
-			f.write(res)
+                name=list(j.keys())[0]
+                print("saving : "+i+" - "+name+"...")
+                AH=j[name]
+                driver.get(AH)
+                page = driver.page_source
+                soup = BeautifulSoup(page,"html.parser")
+                res = soup.text
+                res = res.replace("\n","")
+                res = res.replace("\t","")
+                resjs = json.loads(res)
+                res = json.dumps(resjs)
+                updatetime = int(time.time())
+                title = "data/"+i+"_"+name+"_"+str(updatetime)+".txt"
+                with open(title,"w") as f:
+                        f.write(res)
 
 while True:
-	t1 = time.time()
-	for i in setregion:
+        t1 = time.time()
+        for i in setregion:
                 if i in keys:
-			CacheOne(i)
-	t2 = time.time()
-	dt = t2 - t1
-	wt = 3600.0-dt
-	print(str(dt)+"elapsed...")
-	print("waiting for the next period...")
-	if wt>0:
-		time.sleep(wt)
+                        CacheOne(i)
+        t2 = time.time()
+        dt = t2 - t1
+        wt = 7200.0-dt
+        print(str(dt)+"elapsed...")
+        print("waiting for the next period...")
+        if wt>0:
+                time.sleep(wt)
+                
+driver.quit()
